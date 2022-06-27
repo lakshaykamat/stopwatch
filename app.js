6891a26ebae6aed228a759fa794a77f5a8abf7da
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-28 00:22:20 +0530

    font chande

--- app.js
+++ app.js
@@ -1,73 +1,76 @@
-let seconds = 00;
-let tens = 00;
-let minute = 00;
-// Getting Query
-let getMinute = document.querySelector('.minute');
-let getSeconds = document.querySelector('.seconds');
-let getTens = document.querySelector('.tens');
-let buttonStart = document.querySelector('.btn-start');
-let buttonStop = document.querySelector('.btn-stop');
-let buttonReset = document.querySelector('.btn-reset');
+let tensEl = document.getElementById('tens')
+let secondsEl = document.getElementById('seconds')
+let minEl = document.getElementById('min')
 
-//Add eventlistner when click start function setInterval
-buttonStart.addEventListener('click', ()=>{
-   interval = setInterval(startTimer,10)
-})
-
-//Add event listner when click the stop button to clear interval
-buttonStop.addEventListener('click',()=>{
-    clearInterval(interval);
-})
-
-// add event listner to rest the time
-buttonReset.addEventListener('click',()=>{
-    clearInterval(interval);
-    tens = '00';
-    seconds = '00';
-    minute = '00';
-    getSeconds.innerHTML = seconds;
-    getTens.innerHTML = tens;
-    getMinute.innerHTML = minute;
-})
-
+let minute = 0
+let seconds = 0
+let tens = 0
+let timer = false;
 function startTimer(){
-    //tens = 0
-    tens++;
-    //tens = 1
-    if(tens <= 9){
-        //loop runs when tens is less than or equal to 9
-        getTens.innerHTML = "0" + tens;
-    }
-    //loop runs when tens is greater than 9 
-    if(tens > 9){
-        getTens.innerHTML = tens;
-    }
-    // if tens is greater than 99 
-    if(tens > 99){
-        // seconds = 0
-        seconds++;
-        // seconds = 1
-        getSeconds.innerHTML = "0" +  seconds;
-        //reseting tens to 0
-        tens = tens * 0;
-        // printing tens is 0
-        getTens.innerHTML = "0" + tens;
-    }
-    // if secons is greater than 9 prints secons as it is
-    if(seconds > 9){
-        getSeconds.innerHTML = seconds;
-    }
-    // if seconds is equal to 59 than add 1 minute
-    if(seconds == 59){
-        // minute = 0
-        minute++;
-        // minute = 1
-        getMinute.innerHTML = "0" + minute;
-        // reseting seconds to 0 by multiplying to 0
-        seconds = seconds * 0;
-        getSeconds.innerHTML = "0" + seconds ;
-    }
-    if(minute >9){
-        getMinute.innerHTML = minute;
-    }
+    timer = true;
+        stopWatch()
 }
+function stopTimer(){
+    clearInterval(interval);
+    timer = false;
+}
+function resetTimer(){
+    timer = false
+    clearInterval(interval);
+    tens = 0
+    minute = 0
+    seconds = 0
+    tensEl.textContent = "0" + tens
+    minEl.textContent = "0" + minute
+    secondsEl.textContent = "0" + seconds
+}
+function stopWatch(){
+    //if condition true below code will execute
+    if(timer === true){
+
+        // if timer is true then tens wil increment by 1
+        tens++
+        // if tens will be less than or equal to 9 than add 0 as a string
+        if(tens <= 9){
+            tensEl.textContent = "0" +  tens
+        }
+        // if tens will be greater than or equal to 10 than remove 0 that added above
+        if(tens >= 10){
+            tensEl.textContent = tens
+        }
+        // if tens is equal to 99 than increment second by 1 and tens 0
+
+      //    SECONDS
+        if(tens == 99){
+            tens = 0
+            seconds++
+            //if seconds will be less than or equal to 9 than add 0 as a string
+            if(seconds <= 9){
+                secondsEl.textContent = "0" + seconds
+            }
+            // if seconds will be greater than or equal to 10 than remove 0 that added above
+            if(seconds >= 10 ){
+                secondsEl.textContent = seconds
+            }
+            // if seconds is equal to 99 than increment minute by 1 and tens 0
+            
+            //  MINUTE
+            if(seconds === 59){
+                seconds = 0
+                minute++
+                //if minute will be less than or equal to 9 than add 0 as a string
+                if(minute <= 9){
+                    minEl.textContent = "0" + minute
+                }
+                // if minute will be greater than or equal to 10 than remove 0 that added above
+                if(minute >= 10){
+                    minEl.textContent = minute
+                }
+            }
+        }
+        interval = setTimeout(startTimer, 10)
+    }
+    else{
+        console.log("SToped")
+    }
+    }
