1f2d464ff9f0c7c0890bb472e605d853f0d9e63c
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-06-28 10:46:32 +0530

    new update

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
+//Selecting spans
 let tensEl = document.getElementById('tens')
 let secondsEl = document.getElementById('seconds')
 let minEl = document.getElementById('min')
-
+let buttonDiv = document.getElementById('btn-div')
+// Selectinh Buttons
+let startBtn = document.getElementById('start')
+let stopBtn = document.getElementById('stop')
+let resetBtn = document.getElementById('reset')
+//creating variable
 let minute = 0
 let seconds = 0
 let tens = 0
 let timer = false;
+function buttonOp(){
+        createStopBtn()
+}
+function createStopBtn(){
+    startBtn.remove()
+    let stop = document.createElement('button')
+    stop.textContent = "STOP"
+    buttonDiv.prepend(stop)
+    stop.onclick = function(){
+        stopTimer()
+        stop.remove()
+        createStartBtn()
+    }
+}
+function createStartBtn(){
+    let start  = document.createElement('button')
+    start.textContent = "START"
+    buttonDiv.prepend(start)
+    start.onclick = function(){
+        start.remove()
+        createStopBtn()
+        startTimer()
+    }
+}
 function startTimer(){
     timer = true;
-        stopWatch()
+    stopWatch()
 }
 function stopTimer(){
-    clearInterval(interval);
     timer = false;
+    clearInterval(interval);
 }
 function resetTimer(){
     timer = false
-    clearInterval(interval);
     tens = 0
     minute = 0
     seconds = 0
     tensEl.textContent = "0" + tens
     minEl.textContent = "0" + minute
     secondsEl.textContent = "0" + seconds
+    clearInterval(interval)
 }
 function stopWatch(){
     //if condition true below code will execute
@@ -41,7 +71,7 @@ function stopWatch(){
         // if tens is equal to 99 than increment second by 1 and tens 0
 
       //    SECONDS
-        if(tens == 99){
+        if(tens === 99){
             tens = 0
             seconds++
             //if seconds will be less than or equal to 9 than add 0 as a string
@@ -69,8 +99,5 @@ function stopWatch(){
             }
         }
         interval = setTimeout(startTimer, 10)
-    }
-    else{
-        console.log("SToped")
     }
     }
